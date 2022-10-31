0e3e7ad6d0fbc55775ac41968b37697c11ff9f59
lopesdea91 <lopesdea91@gmail.com>
2022-10-31 07:39:08 -0300

    update types

---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+import { ApiPokemon, Pokemon, PokemonResponse } from './pokemon'
+
+export type {
+	ApiPokemon,
+	Pokemon,
+	PokemonResponse,
+}
---
+++ src/types/pokemon.ts
@@ -0,0 +1,38 @@
+export type ApiPokemon = {
+	name: string;
+	url: string;
+};
+
+type PokemonAbility = {
+	name: string, url: string
+}
+export type PokemonSprites = {
+	back_default: string | null
+	back_female: string | null
+	back_shiny: string | null
+	back_shiny_female: string | null
+	front_default: string | null
+	front_female: string | null
+	front_shiny: string | null
+	front_shiny_female: string | null
+}
+type PokemonType = {
+	name: string, url: string
+}
+type PokemonCommun = {
+	id: number
+	name: string
+	order: number
+	height: number
+	weight: number
+}
+export interface PokemonResponse extends PokemonCommun {
+	abilities: { ability: PokemonAbility }[],
+	types: { type: PokemonType }[]
+}
+export interface Pokemon extends PokemonCommun {
+	abilities: PokemonAbility[],
+	types: PokemonType[],
+	type: string | null,
+	imageUrl: string
+};
